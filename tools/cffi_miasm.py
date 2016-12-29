5d04436432f5afc6f5b1c5e4512774669351d276
Guillaume Valadon <guillaume@valadon.net>
2016-12-29 12:11:28 +0100

    Argument to dump the generated C code

--- tools/cffi_miasm.py
+++ tools/cffi_miasm.py
@@ -2,29 +2,44 @@
 # Copyright (C) 2016 Guillaume Valadon <guillaume@valadon.net>
 
 """
-Generate the r2m2 library
+Generate the miasm embedded library
 """
 
 import sys
 import cffi
+import argparse
 
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="generate the miasm library")
+parser.add_argument("--dump", action="store_true",
+                    dest="dump", default=False)
+parser.add_argument("plugin_name", help="r2m2 plugin name")
+args = parser.parse_args()
+
+# Create the FFI object
 ffi = cffi.FFI()
 
-plugin_name = sys.argv[1]
-
-ffi.set_source("miasm_embedded_%s" % plugin_name, """
+# Declare the library name
+ffi.set_source("miasm_embedded_%s" % args.plugin_name, """
 #include "src/r2m2.h"
 """)
 
+# Parse include files to get functions that will be exported
 includes = "".join(open("src/r2m2.h").readlines())
-includes += "".join(open("src/%s.h" % plugin_name).readlines())
+includes += "".join(open("src/%s.h" % args.plugin_name).readlines())
 ffi.embedding_api(includes)
 
+# libc functions that will be used from Python
 ffi.cdef("""
 void *malloc(size_t size);
 char *strncpy(char *dest, const char *src, size_t n);
 """)
 
-ffi.embedding_init_code("".join(open("src/%s_cffi.py" % plugin_name).readlines()))
+# Python code that will be embedded
+ffi.embedding_init_code("".join(open("src/%s_cffi.py" % args.plugin_name).readlines()))
 
+# Compile the library
+if args.dump:
+  ffi.emit_c_code("miasm_embedded_%s.c" % args.plugin_name)
 ffi.compile(verbose=True)
