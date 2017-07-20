68eaf390a3e9ea653140b634a35a19732fc0ed20
Guillaume Valadon <guillaume@valadon.net>
2017-07-20 09:13:52 +0200

    Incorrect prototype fixed

--- src/r2m2_Ae.h
+++ src/r2m2_Ae.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 Guillaume Valadon <guillaume@valadon.net>
+// Copyright (C) 2017 Guillaume Valadon <guillaume@valadon.net>
 
 void miasm_anal(RAnalOp_r2m2*, unsigned long long, const unsigned char*, int);
-char* miasm_get_reg_profile();
+char* miasm_get_reg_profile(void);
